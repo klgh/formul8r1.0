0d0aaf2603c6c796ee45545e031e4302e61ac8fc
klghlch <kaleighleach@gmail.com>
2013-11-19 15:26:27 -0500

    login/sign up pages

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,9 @@
+class LoginController < ApplicationController
+
+  def login
+  end
+  
+  def signup
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Formul8r10::Application.routes.draw do
 
+  get "login/login"
+
   controller :home do
     get 'features' => :features
     get 'pricing' => :pricing
     get 'support' => :support
   end
+
+  controller :login do
+  	get 'login' => :login
+  	get 'signup' => :signup
+  end
+
 
   root :to => 'home#index'
 
