d98c64a2c3347e022813f805a6f924334b2b78ef
klghlch <kaleighleach@gmail.com>
2013-12-09 16:30:25 -0500

    Password page

--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,27 @@ Formul8r10::Application.routes.draw do
   get "account/share"
   get "account/createjob"
 
+  get "login/password"
+
   controller :home do
     get 'features' => :features
     get 'pricing' => :pricing
     get 'support' => :support
     get 'faq' => :faq
+    get 'index' => :index
   end
 
   controller :login do
   	get 'signin' => :signin
   	get 'signup' => :signup
+    get 'password' => :password
   end
 
   controller :account do
-    get 'index' => :index
+    get 'account' => :index
     get 'templates' => :templates
     get 'share' => :share
+    get 'createjob' => :createjob
   end 
 
 
